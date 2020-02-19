2eaff48ea9d9242553d0a765c9883efac2a66947
TheGhost902 <emilc902@gmail.com>
2020-02-19 23:53:28 +0300

    in Routine model change failed field to done

--- models/Routine.js
+++ models/Routine.js
@@ -15,7 +15,7 @@ const routineSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    failed: {
+    done: {
         type: [Date],
         required: true,
         default: []
